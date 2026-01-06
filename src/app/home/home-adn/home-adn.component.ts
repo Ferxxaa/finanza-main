3aa5f82d48451f82e0989bf276a405569b275c26
Ferxxaa <gonzalezfernanda491@gmail.com>
2026-01-06 13:33:56 -0300

    fixup!  Commit message template

--- src/app/home/home-adn/home-adn.component.ts
+++ src/app/home/home-adn/home-adn.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '../../../../node_modules/@angular/core';
+import { AfterViewInit, Component, OnInit } from '../../../../node_modules/@angular/core';
 
 @Component({
   selector: 'app-home-adn',
   templateUrl: './home-adn.component.html',
   styleUrls: ['./home-adn.component.css']
 })
-export class HomeAdnComponent implements OnInit {
+export class HomeAdnComponent implements OnInit, AfterViewInit {
 
   Titulo:string;
 
@@ -14,6 +14,18 @@ export class HomeAdnComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // Compatibilidad SVG2: algunos navegadores prefieren `href` en vez de `xlink:href`.
+    // Copiamos el valor sin duplicar el Base64 en el HTML.
+    const images = Array.from(document.querySelectorAll('svg image')) as SVGImageElement[];
+    for (const img of images) {
+      const xlink = img.getAttribute('xlink:href');
+      if (xlink && !img.getAttribute('href')) {
+        img.setAttribute('href', xlink);
+      }
+    }
   }
 
 }
